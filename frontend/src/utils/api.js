b86f8d8924267c9e97a9ce7a73b10f636c585065
Saheb Bhattacharjee <sahebbhattacherjee129@gmail.com>
2026-05-31 22:34:29 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,6 @@
-const BASE_URL = '/api';
+const BASE_URL =  import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : '/api';
 
 const getToken = () => localStorage.getItem('lms_token');
 
